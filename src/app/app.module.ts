a3519ae7207f639699c61d40c9b518bb63e3b1b3
Brunno Borges <brunnoborges@trabalhabrasil.com.br>
2025-06-11 16:10:24 -0300

    add - ngx-translate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
 import { WeaponListComponent } from './components/weapon-list/weapon-list.component';
+
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { MatCardModule } from '@angular/material/card';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatDividerModule } from '@angular/material/divider';
@@ -14,13 +16,26 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+// Função para carregar os arquivos de tradução
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
 @NgModule({
-  declarations: [AppComponent, WeaponListComponent],
+  declarations: [
+    AppComponent,
+    WeaponListComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
+
+    // Angular Material
     MatCardModule,
     MatExpansionModule,
     MatDividerModule,
@@ -28,8 +43,18 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     MatPaginatorModule,
     MatInputModule,
     MatFormFieldModule,
+
+    // ngx-translate
+    TranslateModule.forRoot({
+      defaultLanguage: 'pt',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/components/weapon-list/weapon-list.component.ts
+++ src/app/components/weapon-list/weapon-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { EldenRingService } from 'src/app/services/elden-ring.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-weapon-list',
@@ -15,14 +16,39 @@ export class WeaponListComponent implements OnInit {
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  constructor(private eldenRingService: EldenRingService) {}
+  constructor(
+    private eldenRingService: EldenRingService,
+    private translate: TranslateService
+  ) {}
 
   ngOnInit(): void {
+    this.translate.setDefaultLang('pt'); // Define o idioma padrão
+
     this.eldenRingService.getWeapons(100).subscribe((response) => {
-      this.dataSource.data = response.data;
+      const translatedData = response.data.map((weapon: any) => ({
+        ...weapon,
+        category: this.translate.instant('CATEGORIES.' + weapon.category) || weapon.category,
+        requiredAttributes: weapon.requiredAttributes?.map((attr: any) => ({
+          ...attr,
+          name: this.translate.instant('ATTRIBUTES.' + attr.name) || attr.name
+        })),
+        scalesWith: weapon.scalesWith?.map((scale: any) => ({
+          ...scale,
+          name: this.translate.instant('ATTRIBUTES.' + scale.name) || scale.name
+        })),
+        attack: weapon.attack?.map((atk: any) => ({
+          ...atk,
+          name: this.translate.instant('ATTRIBUTES.' + atk.name) || atk.name
+        })),
+        defence: weapon.defence?.map((def: any) => ({
+          ...def,
+          name: this.translate.instant('ATTRIBUTES.' + def.name) || def.name
+        }))
+      }));
+
+      this.dataSource.data = translatedData;
       this.dataSource.paginator = this.paginator;
 
-      // Adiciona filtro customizado que verifica campos aninhados
       this.dataSource.filterPredicate = (weapon, filter: string) => {
         const textToSearch = [
           weapon.name,
